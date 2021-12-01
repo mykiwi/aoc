b175066bdb3081f2fe42df4790a1923323699f4b
Romain Gautier <mail@romain.sh>
2021-12-01 20:18:00 +0000

    add day1 in php

--- 2021/01.php/main.part2.php
+++ 2021/01.php/main.part2.php
@@ -6,13 +6,7 @@ $numbers = \array_map('intval', \explode("\n", $raw));
 $sum = 0;
 $previous = null;
 foreach ($numbers AS $i => $number) {
-    $offset = $i - 2;
-    $length = 3;
-    while ($offset < 0) {
-        $offset++;
-        $length--;
-    }
-    $current = \array_sum(\array_slice($numbers, $offset, $length));
+    $current = \array_sum(\array_slice($numbers, $i, 3));
 
     if ($previous && $previous < $current) {
         $sum++;
