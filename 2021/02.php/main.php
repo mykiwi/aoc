9c4d93141093ab4f635131768c6ed1c9c3f41324
Romain Gautier <mail@romain.sh>
2021-12-02 13:06:40 +0000

    better wording

--- 2021/02.php/main.php
+++ 2021/02.php/main.php
@@ -3,11 +3,11 @@
 $raw = \file_get_contents('./input');
 $depth = 0;
 
-preg_match_all('#down (\d+)#', $raw, $up);
-preg_match_all('#up (\d+)#', $raw, $down);
+preg_match_all('#down (\d+)#', $raw, $down);
+preg_match_all('#up (\d+)#', $raw, $up);
 preg_match_all('#forward (\d+)#', $raw, $forward);
-$depth += \array_sum($up[1]);
-$depth -= \array_sum($down[1]);
+$depth += \array_sum($down[1]);
+$depth -= \array_sum($up[1]);
 $horizontal = \array_sum($forward[1]);
 
 echo $depth * $horizontal;
